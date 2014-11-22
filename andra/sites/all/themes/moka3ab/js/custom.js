904482bd6e9b4a98037559fd6263e21974475e6f
Mouad <mouad@inspheris.com>
2014-11-22 03:14:49 +0000

    First Push for Andra
    First Push for Andra Project

--- andra/sites/all/themes/moka3ab/js/custom.js
+++ andra/sites/all/themes/moka3ab/js/custom.js
@@ -1,20 +1,6 @@
 ﻿(function($){
 	$(window).load(function() {
-		$('#slider').nivoSlider({
-			pauseTime: 5000,
-			effect: 'fade',
-			pauseOnHover: false,
-			controlNav: false,
-			directionNav: false
-		});
-		
-		$(".not-front .block-views-latest-offers-block .content").slideUp();
-		$(".context-ville h1").fadeIn(3000);
-		$( ".not-front .block-views-latest-offers-block h2" ).click(function() {
-			$(".not-front .block-views-latest-offers-block .content").slideToggle();
-			$(".context-ville h1").fadeToggle();
-		});
-		
+		$('#views-exposed-form-chercher-page input[type="text"]').attr('placeholder','Rechercher');
 		
 	});
  })(jQuery)
